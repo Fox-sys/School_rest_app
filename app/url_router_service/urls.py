913c6ca66e3c8c778886c724f2b3ce87afc2a8f1
fox-sys <borisberestov@ya.ru>
2021-02-12 12:01:40 +0300

    added urls.py files (v0.2.1)

--- app/url_router_service/urls.py
+++ app/url_router_service/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    
+    path("comms/", include('comm_service.urls')),
+    path("diary/", include('diary_service.urls')),
+    path("news/", include('news_service.urls')),
+    path("profile/", include('profile_service.urls')),
+    path("report/", include('report_service.urls')),
 ]
